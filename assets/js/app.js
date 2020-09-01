3e70a2ffe92cfa7706d11ea9abd413d2b02ce52b
xpedrotavares <xpedrotavaresdev@gmail.com>
2020-09-01 17:03:25 -0300

    player moves to right, left and jumps

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,195 +1,214 @@
- const canvas = document.getElementById('gameCanvas');
- const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+// variaveis
+
+let player;
+let gravity;
+let keys = {};
+let obstacles = [];
+
+// fazer os isteners
+document.addEventListener("keydown", function (evt) {
+  keys[evt.code] = true;
+});
+document.addEventListener("keyup", function (evt) {
+  keys[evt.code] = false;
+});
+
+class Player {
+  constructor(x, y, w, h, c) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.c = c;
+
+    this.dy = 0;
+    this.jumpForce = 15;
+    this.originalHeight = h;
+    this.grounded = false;
+    this.jumpTimer = 0;
+  }
+
+  Animate() {
+    // controles
+    if (keys["Space"] || keys["KeyW"]) {
+      this.Jump();
+    } else {
+      this.jumpTimer = 0;
+    }
+
+    if (keys['ArrowLeft']){
+        this.x -= 4; 
+    } 
+
+    if (keys['ArrowRight']){
+        this.x += 4;
+
+    }
+    this.y += this.dy;
+
+    // Gravity
+    if (this.y + this.h < canvas.height) {
+      this.dy += gravity;
+      this.grounded = false;
+    } else {
+      this.dy = 0;
+      this.grounded = true;
+      this.y = canvas.height - this.h;
+    }
+
+    this.Draw();
+  }
+
+  Jump() {
+    if (this.grounded && this.jumpTimer == 0) {
+      this.jumpTimer = 1;
+      this.dy = -this.jumpForce;
+    } else if (this.jumpTimer > 0 && this.jumpTimer < 15) {
+      this.jumpTimer++;
+      this.dy = -this.jumpForce - this.jumpTimer / 50;
+    }
+  }
+
+  Draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.c;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+    ctx.closePath();
+  }
+}
+
+
+
+function Start() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  gravity = 1;
+
+  player = new Player(25, 0, 50, 50, "#FF5858");
+
+  requestAnimationFrame(Update);
+}
+
+function Update() {
+  requestAnimationFrame(Update);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  player.Animate();
+}
+
 
  
-//medias e posições no canvas dos steps
-const steps = [];
-//desenho dos steps
+Start();
 
-    // ctx.fillStyle = "#000000"
-    // ctx.fillRect(300, 700, 200, 20);
+// -----------------------------------------------
 
+// // canvas
+// const canvas = document.getElementById('game');
+// const ctx = canvas.getContext('2d');
 
-//personagem => passar para uma const que fara via Class component linha 109
+// // variaveis
+
+// let score;
+// let highscore;
 // let player;
-// ctx.fillStyle = "#0000FF";
-// ctx.fillRect(0, 760, 40, 40);
+// let gravity;
+// let obstacles;
+// let gameSpeed;
+// let keys = {};
 
+// // listeners
+// document.addEventListener('keydown', function (evt) {
+//     keys[evt.code] = true;
+//   });
+//   document.addEventListener('keyup', function (evt) {
+//     keys[evt.code] = false;
+//   });
+// //desenhar player
 
-const gameArea = {
-    canvasWidth: 500,
-    canvasHeight: 800,
-    canvas: canvas,
-    frames: 0,
-    star: function () {
-        this.interval = setInterval(updateGameArea, 20)
-    },
-    stop: function () {
-        clearInterval(this.interval);
-    },
-    clear: function ( ){
-        ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-    },
-    // score: function () {
-        
-    // }
-
-}
-
-function updateGameArea() {
-    gameArea.clear();
-    player.newPos(); //ainda nao declarei o player
-    player.update(); // ainda nao declarei o player
-    updateObstacles(); // ainda nao declarei func updateObstacles()
-    checkGameOver(); //// ainda nao declarei essa func
-    // myGameArea.score();
-}
-
-function updateObstacles() {
-    gameArea.frames += 1;
-    if(gameArea.frames % 120 === 0) {
-        let y = gameArea.canvasHeight;
-        let minWidth = 20;
-        let maxWidth = 200;
-        let width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
-        let minGap = 50;
-        let maxGap = 200;
-        let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
-        steps.push(new Component(width, 100, 'green', 0, y));
-        steps.push( new Component(y - width - gap, 100, 'green', width + gap, y));
-
-        // constructor(width, height, color, x, y)
-    }
-
-    for (i=0; i < steps.length; i++) {
-        steps[i].x += -1;
-        steps[i].update();
-    }
-}
-
-function checkGameOver() {
-    const crashed = steps.some(function (obstacle) {
-        return player.crashWith(obstacle);
-    });
-
-    if (crashed) {
-        gameArea.stop();
-        alert('game over')
-    }
-}
-
-class Component {
-    constructor(width, height, color, x, y) {
-        this.width = width;
-        this.height = height;
-        this.color = color;
-        this.x = x;
-        this.y = y;
-        this.speedX = 0;
-        this.speedY = 0;
-    }
-
-    left() {
-        return this.x;
-    }
-    right() {
-        return this.x + this.width;
-    }
-    top() {
-        return this.y;
-    }
-    bottom () {
-        return this.y + this.height
-    }
-
-    crashWith(obstacle) {
-        return !(
-            this.bottom() < obstacle.top() ||
-            this.top() > obstacle.bottom() ||
-            this.right() < obstacle.left() ||
-            this.left() > obstacle.right()
-        );
-    }
-
-    update() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height)
-    }
-
-    newPos() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-    }
-}
-
-const player = new Component(40, 40, "blue", 0, 760)
-
-gameArea.star();
-
-document.addEventListener("keydown", (e) => {
-    switch (e.keyCode) {
-        case 37:
-            player.speedX -= 1;
-            break;
-        case 39:
-            player.speedX += 1;
-            break;
-        case 32:
-            player.speedY -= 1;
-            break;
-    }
-})
-
-document.addEventListener("keyup", (e) => {
-    player.speedX = 0;
-    player.speedY = 0;
-})
-
-
-//posicao atual do obj x y ... w h, functions => update, clear
-//bottom player = bottom obsta ... zerar velocidade y
-// class step {
-
-//     constructor (x, y, width, height, color){
+// class Player {
+//     constructor (x, y, width, height, color) {
 //         this.x = x;
 //         this.y = y;
 //         this.width = width;
 //         this.height =height;
 //         this.color = color;
+
+//         this.dy = 0;
+//         this.jumpForce = 15;
+//         this.originalHeight = height;
+//         this.grounded = false;
+//         this.jumpTimer = 0;
 //     }
 
-//     draw(){
+//     Animate () {
+//         //jump
+//         if (keys['Space'] || keys['KeyW']) {
+//             this.Jump();
+//             // console.log('jump')
+//         } else {
+//             this.jumpTimer = 0;
+//         }
+
+//         this.y  += this.dy;
+
+//         //gravity
+//         if (this.y + this.height < canvas.height){
+//             this.dy += gravity;
+//             this.grounded = false;
+//         }else {
+//             this.dy = 0;
+//             this.y = canvas.height - this.height;
+//         }
+
+//         this.Draw();
+//     }
+
+//     Jump () {
+//         if (this.grounded && this.jumpTimer == 0) {
+//           this.jumpTimer = 1;
+//           this.dy = -this.jumpForce;
+//         } else if (this.jumpTimer > 0 && this.jumpTimer < 15) {
+//           this.jumpTimer++;
+//           this.dy = -this.jumpForce - (this.jumpTimer / 50);
+//         }
+//     }
+
+//     Draw () {
 //         ctx.beginPath();
-//         ctx.lineWidth = '4';
-//         ctx.fillStyle = "#FFFFFF";
-//         ctx.fillRect(50, 50, 40, 50);
+//         ctx.fillStyle = this.color;
+//         ctx.fillRect(this.x, this.y, this.width, this.height);
+//         ctx.closePath();
 //     }
 // }
 
-// function startGame() {
+// function Start() {
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight;
 
+//     ctx.font = '20px';
+
+//     gameSpeed = 3;
+//     gravity = 1;
+
+//     score = 0;
+//     highscore = 0;
+
+//     //criando um player
+// player = new Player(25, canvas.height - 150, 50, 50, "#0000FF")
+
+// requestAnimationFrame(Update)
 // }
 
-// window.onload = function () {
-//     document.getElementById("")
+// // Limpa os frames para dar a impressao de que o rect tá se movendo
+// function Update() {
+//     requestAnimationFrame(Update);
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//     player.Animate();
 // }
 
-
-// const { start } = require("repl");
-
-// const gameArea = {
-//  canvas: document.createElement("canvas"),
-//  steps: [],
-//  frames: 0,
-//  gravity: 0.1,
-//  drawCanvas: function () {
-//      this.canvas.width = 500;
-//      this.canvas.height = 800;
-//      this.ctx = this.canvas.getContext("2d");
-//  },
-//  start: function () {
-//      this.drawCanvas();
-
-//      this.reqAnimation = window.requestAnimationFrame()
-//  }
-// }
+// Start()
