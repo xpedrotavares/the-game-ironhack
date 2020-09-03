c3a6cb7ea75369c98e45f2c8ab272221e5dfaa30
xpedrotavares <xpedrotavaresdev@gmail.com>
2020-09-03 11:41:12 -0300

    Remodulacao do projeto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,99 +1,69 @@
+window.onload = function () {
+
+
+
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 // variaveis
 
-let player;
-let gravity;
-let keys = {};
-let obstacles = [];
-let gameSpeed;
-let currentStep;
 let score;
 let scoreText;
+let highscore;
+let highscoreText;
+let player;
+let gravity;
+let obstacles = [];
+let gameSpeed;
+let keys = {};
 // let steps = [{ width: 300, height: 30, x: canvas.width, y: 0 }];
 // let frame = ;
+
 // fazer os listeners
-document.addEventListener("keydown", function (evt) {
+document.addEventListener('keydown', function (evt) {
   keys[evt.code] = true;
 });
-document.addEventListener("keyup", function (evt) {
+document.addEventListener('keyup', function (evt) {
   keys[evt.code] = false;
 });
 
 //descrever melhor os parametros
 class Player {
-  constructor(x, y, w, h, c) {
+  constructor (x, y, w, h) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
-    this.c = c;
+    
 
+    this.img = new Image()
+    this.img.src = './assets/images/dino01.jpg'
+    
     this.dy = 0;
     this.jumpForce = 15;
-    
+    this.originalHeight = h;
     this.grounded = false;
     this.jumpTimer = 0;
-    this.currentStep;
-    this.isAboveStep = false;
   }
 
-  left() {
-    return this.x;
-  }
-  right() {
-    return this.x + this.w;
-  }
-  top() {
-    return this.y;
-  }
-  bottom() {
-    return this.y + this.h;
-  }
-
-  stepOver(obstacle) {
-    const stepedOver =
-      this.bottom() > obstacle.top() &&
-      // this.top() > obstacle.bottom() ||
-      this.right() < obstacle.right() &&
-      this.left() > obstacle.left();
-    this.currentStep = obstacle;
-    if (stepedOver) {
-      this.isAboveStep = true;
-    } else {
-      this.isAboveStep = false;
-    }
-    
-
-    //para cada frame atualizar o Y no update.
-    // player.y = current
-  }
-
-  Animate() {
-    // controles
-    if (keys["Space"] || keys["KeyW"]) {
+  Animate () {
+    // Jump
+    if (keys['Space'] || keys['KeyW']) {
       this.Jump();
     } else {
       this.jumpTimer = 0;
     }
 
-    if (keys["ArrowLeft"]) {
-      this.x -= 4;
+    if (keys['ShiftLeft'] || keys['KeyS']) {
+      this.h = this.originalHeight / 2;
+    } else {
+      this.h = this.originalHeight;
     }
 
-    if (keys["ArrowRight"]) {
-      this.x += 4;
-    }
     this.y += this.dy;
 
-    // Gravity
-    
-    if (this.isAboveStep) {
-      console.log(this.currentStep)
-      this.dy = this.currentStep.y + 5;
-    
-    } else if (this.y + this.h < canvas.height) {
+    // Gravidade
+    if (this.y + this.h < canvas.height) {
       this.dy += gravity;
       this.grounded = false;
     } else {
@@ -101,60 +71,48 @@ class Player {
       this.grounded = true;
       this.y = canvas.height - this.h;
     }
-    
-
 
     this.Draw();
   }
 
-  Jump() {
+  Jump () {
     if (this.grounded && this.jumpTimer == 0) {
       this.jumpTimer = 1;
       this.dy = -this.jumpForce;
     } else if (this.jumpTimer > 0 && this.jumpTimer < 15) {
       this.jumpTimer++;
-      this.dy = -this.jumpForce - this.jumpTimer / 50;
+      this.dy = -this.jumpForce - (this.jumpTimer / 50);
     }
   }
 
-  Draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.c;
-    ctx.fillRect(this.x, this.y, this.w, this.h);
-    ctx.closePath();
-  }
+  Draw () {
+  
+    
+    ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+  } 
+   
+   
+  
 }
 
 class Obstacle {
-  constructor(x, y, w, h, c) {
+  constructor (x, y, w, h, c) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
     this.c = c;
 
-    this.dy = -gameSpeed;
+    this.dx = -gameSpeed;
   }
 
-  left() {
-    return this.x;
-  }
-  right() {
-    return this.x + this.w;
-  }
-  top() {
-    return this.y;
-  }
-  bottom() {
-    return this.y + this.h;
-  }
-
-  Update() {
-    this.y -= this.dy;
+  Update () {
+    this.x += this.dx;
     this.Draw();
-    this.dy = -gameSpeed;
+    this.dx = -gameSpeed;
   }
-  Draw() {
+
+  Draw () {
     ctx.beginPath();
     ctx.fillStyle = this.c;
     ctx.fillRect(this.x, this.y, this.w, this.h);
@@ -181,57 +139,61 @@ class Text {
   }
 }
 
-function SpawnObstacle() {
-  let obstaclePositionX = RandomX(0, 900);
+
+//funções
+
+function SpawnObstacle () {
   let size = RandomIntInRange(20, 70);
   let type = RandomIntInRange(0, 1);
-  let obstacle = new Obstacle(100, 0, 200, 20, "#00FF00");
+  let obstacle = new Obstacle(canvas.width + size, canvas.height - size, size, size, '#2484E4');
 
-  obstacle.x = obstaclePositionX;
-
+  if (type == 1) {
+    obstacle.y -= player.originalHeight - 10;
+  }
   obstacles.push(obstacle);
 }
 
-SpawnObstacle();
 
-function RandomX(min, max) {
+function RandomIntInRange (min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
-function RandomIntInRange(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
+function Start () {
+  canvas.width = 700;
+  canvas.height = 700;
 
-function Start() {
-  canvas.width = 900;
-  canvas.height = window.innerHeight * 0.9;
+  ctx.font = "20px sans-serif";
 
   gameSpeed = 3;
   gravity = 1;
 
   score = 0;
+  highscore = 0;
+  if (localStorage.getItem('highscore')) {
+    highscore = localStorage.getItem('highscore');
+  }
 
-  player = new Player(25, 0, 50, 50, "#FF5858");
-  //   steps = new Steps(25, 40, 100, 150, "#00FF00");
+  player = new Player(25, 0, 50, 50);
 
-scoreText = new Text("Score: " + score, 25, 25, 'left', '#0000FF', '20')
+  scoreText = new Text("Score: " + score, 25, 25, "left", "#212121", "20");
+  highscoreText = new Text("Highscore: " + highscore, canvas.width - 25, 25, "right", "#212121", "20");
 
   requestAnimationFrame(Update);
 }
 
+
 let initialSpawnTimer = 200;
 let spawnTimer = initialSpawnTimer;
-
-function Update() {
+function Update () {
+  requestAnimationFrame(Update);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  player.Animate();
 
   spawnTimer--;
   if (spawnTimer <= 0) {
     SpawnObstacle();
+    console.log(obstacles);
     spawnTimer = initialSpawnTimer - gameSpeed * 8;
-
+    
     if (spawnTimer < 60) {
       spawnTimer = 60;
     }
@@ -240,67 +202,46 @@ function Update() {
   //aqui vai gerar na tela os obstacles
   for (let i = 0; i < obstacles.length; i++) {
     let o = obstacles[i];
+
+    if (o.x + o.w < 0) {
+      obstacles.splice(i, 1);
+    }
+
+    if (
+      player.x < o.x + o.w &&
+      player.x + player.w > o.x &&
+      player.y < o.y + o.h &&
+      player.y + player.h > o.y
+    ) {
+      obstacles = [];
+      score = 0;
+      spawnTimer = initialSpawnTimer;
+      gameSpeed = 3;
+      window.localStorage.setItem('highscore', highscore);
+    }
+
     o.Update();
   }
 
-  // if (player.x > canvas.width){
-  //   this.y = player.x - canvas.height
-  // }
- 
-  // width: 300, height: 30, x: canvas.width, y: 0
-  // if (frame % 100 == 0)
-  //   steps.push({
-  //     width: 300,
-  //     height: 30,
-  //     x: canvas.width,
-  //     y: canvas.height * Math.random(),
-  //     speedX: 0.0,
-  //     speedY: 0.0,
-  //   });
-  // steps.forEach((o) => {
-  //   o.y += 4;
+  player.Animate();
 
-  //   ctx.fillRect(o.x, o.y, o.width, o.height);
-  // });
-
-  // stepOver.forEach((step) =>{
-
-  // })
-  // obstacles.forEach((frame) =>{
-  //   player.y += currentStep;
-  // })
-
-
-
-// player.y = playerFrameUpdate;
-
-
-// playerFrameUpdate.Update();
-
-  obstacles.forEach((obstacle) => {
-    player.stepOver(obstacle);
-    
-    //  player.y = currentStep;   
-    
-    
-  });
-  
-  requestAnimationFrame(Update);
-
-  // pl
-  //para cada frame atualizar o Y no update.
-  // player.y = current
-
-  //   steps.Animate();
-  score++
-  scoreText.t = 'Score: ' + score;
+  score++;
+  scoreText.t = "Score: " + score;
   scoreText.Draw();
+
+  if (score > highscore) {
+    highscore = score;
+    highscoreText.t = "Highscore: " + highscore;
+  }
+  
+  highscoreText.Draw();
+
+  gameSpeed += 0.003;
 }
-// requestAnimationFrame(Update);
-
-
 
 Start();
+
+}
 
 // -----------------------------------------------
 
