286d55ae6117977ed8ffed86e91a9862e36ec8ea
xpedrotavares <xpedrotavaresdev@gmail.com>
2020-09-04 00:08:49 -0300

    finally done

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,361 +2,253 @@ window.onload = function () {
 
 
 
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
-
-// variaveis
-
-let score;
-let scoreText;
-let highscore;
-let highscoreText;
-let player;
-let gravity;
-let obstacles = [];
-let gameSpeed;
-let keys = {};
-// let steps = [{ width: 300, height: 30, x: canvas.width, y: 0 }];
-// let frame = ;
-
-// fazer os listeners
-// let start = document.getElementById('start');
-// start.addEventListener(onclick('start'), function (evt) {
-//   star
-// })
-
-document.addEventListener('keydown', function (evt) {
-  keys[evt.code] = true;
-});
-document.addEventListener('keyup', function (evt) {
-  keys[evt.code] = false;
-});
-
-//descrever melhor os parametros
-class Player {
-  constructor (x, y, w, h) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    
-
-    this.img = new Image()
-    this.img.src = '../images/kisspng-tyrannosaurus-t-shirt-dino-t-rex-runner-2-lonely-t-8-bit-5ac1f4e6ee15e6.3215979715226605829752.png'
-    
-    this.dy = 0;
-    this.jumpForce = 8;
-    this.originalHeight = h;
-    this.grounded = false;
-    this.jumpTimer = 0;
-  }
-
-  Animate () {
-    // Jump
-    if (keys['Space'] || keys['KeyW']) {
-      this.Jump();
-    } else {
+  const canvas = document.getElementById("game");
+  const ctx = canvas.getContext("2d");
+  
+  // variaveis
+  
+  let score;
+  let scoreText;
+  let highscore;
+  let highscoreText;
+  let player;
+  let gravity;
+  let obstacles = [];
+  let gameSpeed;
+  let keys = {};
+  // let steps = [{ width: 300, height: 30, x: canvas.width, y: 0 }];
+  // let frame = ;
+  
+  // fazer os listeners
+  // let start = document.getElementById('start');
+  // start.addEventListener(onclick('start'), function (evt) {
+  //   star
+  // })
+  
+  document.addEventListener('keydown', function (evt) {
+    keys[evt.code] = true;
+  });
+  document.addEventListener('keyup', function (evt) {
+    keys[evt.code] = false;
+  });
+  
+  //descrever melhor os parametros
+  class Player {
+    constructor (x, y, w, h) {
+      this.x = x;
+      this.y = y;
+      this.w = w;
+      this.h = h;
+      
+  
+      this.img = new Image()
+      this.img.src = '/assets/images/kisspng-tyrannosaurus-t-shirt-dino-t-rex-runner-2-lonely-t-8-bit-5ac1f4e6ee15e6.3215979715226605829752.png'
+      
+      this.dy = 0;
+      this.jumpForce = 8;
+      this.originalHeight = h;
+      this.grounded = false;
       this.jumpTimer = 0;
     }
-
-    if (keys['ShiftLeft'] || keys['KeyS']) {
-      this.h = this.originalHeight / 2;
-    } else {
-      this.h = this.originalHeight;
-    }
-
-    this.y += this.dy;
-
-    // Gravidade
-    if (this.y + this.h < canvas.height) {
-      this.dy += gravity;
-      this.grounded = false;
-    } else {
-      this.dy = 0;
-      this.grounded = true;
-      this.y = canvas.height - this.h;
-    }
-
-    this.Draw();
-  }
-
-  Jump () {
-    if (this.grounded && this.jumpTimer == 0) {
-      this.jumpTimer = 1;
-      this.dy = -this.jumpForce;
-    } else if (this.jumpTimer > 0 && this.jumpTimer < 15) {
-      this.jumpTimer++;
-      this.dy = -this.jumpForce - (this.jumpTimer / 50);
-    }
-  }
-
-  Draw () {
   
+    Animate () {
+      // Jump
+      if (keys['Space'] || keys['KeyW']) {
+        this.Jump();
+      } else {
+        this.jumpTimer = 0;
+      }
+  
+      if (keys['ShiftLeft'] || keys['KeyS']) {
+        this.h = this.originalHeight / 2;
+      } else {
+        this.h = this.originalHeight;
+      }
+  
+      this.y += this.dy;
+  
+      // Gravidade
+      if (this.y + this.h < canvas.height) {
+        this.dy += gravity;
+        this.grounded = false;
+      } else {
+        this.dy = 0;
+        this.grounded = true;
+        this.y = canvas.height - this.h;
+      }
+  
+      this.Draw();
+    }
+  
+    Jump () {
+      if (this.grounded && this.jumpTimer == 0) {
+        this.jumpTimer = 1;
+        this.dy = -this.jumpForce;
+      } else if (this.jumpTimer > 0 && this.jumpTimer < 15) {
+        this.jumpTimer++;
+        this.dy = -this.jumpForce - (this.jumpTimer / 50);
+      }
+    }
+  
+    Draw () {
     
-    ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
-  } 
-   
-   
-  
-}
-
-class Obstacle {
-  constructor (x, y, w, h) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.img = new Image()
-    this.img.src = '../images/Daco_6127748.png'
+      
+      ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+    } 
      
-
-    this.dx = -gameSpeed;
-  }
-
-  Update () {
-    this.x += this.dx;
-    this.Draw();
-    this.dx = -gameSpeed;
-  }
-
-  Draw () {
-
-    ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
-    // ctx.beginPath();
-    // ctx.fillStyle = this.c;
-    // ctx.fillRect(this.x, this.y, this.w, this.h);
-    // ctx.closePath();
-  }
-}
-
-class Text {
-  constructor (t, x, y, a, c, s){
-    this.t = t;
-    this.x = x;
-    this.y = y;
-    this.a = a;
-    this.c = c;
-    this.s = s;
-  }
-  Draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.c;
-    ctx.font = this.s + "px sans-serif"
-    ctx.textAlign = this.a;
-    ctx.fillText(this.t, this.x, this.y);
-    ctx.closePath();
-  }
-}
-
-
-//funções
-
-function SpawnObstacle () {
-  let size = RandomIntInRange(20, 70);
-  let type = RandomIntInRange(0, 1);
-  let obstacle = new Obstacle(canvas.width + size, canvas.height - size, size, size, '#2484E4');
-
-  if (type == 1) {
-    obstacle.y -= player.originalHeight - 10;
-  }
-  obstacles.push(obstacle);
-}
-
-
-function RandomIntInRange (min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
-
-function Start() {
-  canvas.width = 905;
-  canvas.height = 404;
-
-  ctx.font = "20px sans-serif";
-
-  gameSpeed = 3;
-  gravity = 1;
-
-  score = 0;
-  highscore = 0;
-  if (localStorage.getItem('highscore')) {
-    highscore = localStorage.getItem('highscore');
-  }
-
-  player = new Player(80, 0, 50, 50);
-
-  scoreText = new Text("Score: " + score, 25, 25, "left", "#212121", "20");
-  highscoreText = new Text("Highscore: " + highscore, canvas.width - 25, 25, "right", "#212121", "20");
-
-  requestAnimationFrame(Update);
-}
-
-
-let initialSpawnTimer = 200;
-let spawnTimer = initialSpawnTimer;
-function Update () {
-  requestAnimationFrame(Update);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  spawnTimer--;
-  if (spawnTimer <= 0) {
-    SpawnObstacle();
-    console.log(obstacles);
-    spawnTimer = initialSpawnTimer - gameSpeed * 8;
+     
     
-    if (spawnTimer < 60) {
-      spawnTimer = 60;
-    }
-  }
-
-  //aqui vai gerar na tela os obstacles
-  for (let i = 0; i < obstacles.length; i++) {
-    let o = obstacles[i];
-
-    if (o.x + o.w < 0) {
-      obstacles.splice(i, 1);
-    }
-
-    if (
-      player.x < o.x + o.w &&
-      player.x + player.w > o.x &&
-      player.y < o.y + o.h &&
-      player.y + player.h > o.y
-    ) {
-      obstacles = [];
-      score = 0;
-      spawnTimer = initialSpawnTimer;
-      gameSpeed = 3;
-      window.localStorage.setItem('highscore', highscore);
-    }
-
-    o.Update();
-  }
-
-  player.Animate();
-
-  score++;
-  scoreText.t = "Score: " + score;
-  scoreText.Draw();
-
-  if (score > highscore) {
-    highscore = score;
-    highscoreText.t = "Highscore: " + highscore;
   }
   
-  highscoreText.Draw();
-
-  gameSpeed += 0.003;
-}
-
-Start();
-
-}
-
-// -----------------------------------------------
-
-// // canvas
-// const canvas = document.getElementById('game');
-// const ctx = canvas.getContext('2d');
-
-// // variaveis
-
-// let score;
-// let highscore;
-// let player;
-// let gravity;
-// let obstacles;
-// let gameSpeed;
-// let keys = {};
-
-// // listeners
-// document.addEventListener('keydown', function (evt) {
-//     keys[evt.code] = true;
-//   });
-//   document.addEventListener('keyup', function (evt) {
-//     keys[evt.code] = false;
-//   });
-// //desenhar player
-
-// class Player {
-//     constructor (x, y, width, height, color) {
-//         this.x = x;
-//         this.y = y;
-//         this.width = width;
-//         this.height =height;
-//         this.color = color;
-
-//         this.dy = 0;
-//         this.jumpForce = 15;
-//         this.originalHeight = height;
-//         this.grounded = false;
-//         this.jumpTimer = 0;
-//     }
-
-//     Animate () {
-//         //jump
-//         if (keys['Space'] || keys['KeyW']) {
-//             this.Jump();
-//             // console.log('jump')
-//         } else {
-//             this.jumpTimer = 0;
-//         }
-
-//         this.y  += this.dy;
-
-//         //gravity
-//         if (this.y + this.height < canvas.height){
-//             this.dy += gravity;
-//             this.grounded = false;
-//         }else {
-//             this.dy = 0;
-//             this.y = canvas.height - this.height;
-//         }
-
-//         this.Draw();
-//     }
-
-//     Jump () {
-//         if (this.grounded && this.jumpTimer == 0) {
-//           this.jumpTimer = 1;
-//           this.dy = -this.jumpForce;
-//         } else if (this.jumpTimer > 0 && this.jumpTimer < 15) {
-//           this.jumpTimer++;
-//           this.dy = -this.jumpForce - (this.jumpTimer / 50);
-//         }
-//     }
-
-//     Draw () {
-//         ctx.beginPath();
-//         ctx.fillStyle = this.color;
-//         ctx.fillRect(this.x, this.y, this.width, this.height);
-//         ctx.closePath();
-//     }
-// }
-
-// function Start() {
-//     canvas.width = window.innerWidth;
-//     canvas.height = window.innerHeight;
-
-//     ctx.font = '20px';
-
-//     gameSpeed = 3;
-//     gravity = 1;
-
-//     score = 0;
-//     highscore = 0;
-
-//     //criando um player
-// player = new Player(25, canvas.height - 150, 50, 50, "#0000FF")
-
-// requestAnimationFrame(Update)
-// }
-
-// // Limpa os frames para dar a impressao de que o rect tá se movendo
-// function Update() {
-//     requestAnimationFrame(Update);
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//     player.Animate();
-// }
-
-// Start()
+  class Obstacle {
+    constructor (x, y, w, h) {
+      this.x = x;
+      this.y = y;
+      this.w = w;
+      this.h = h;
+      this.img = new Image()
+      this.img.src = '/assets/images/Daco_6127748.png'
+       
+  
+      this.dx = -gameSpeed;
+    }
+  
+    Update () {
+      this.x += this.dx;
+      this.Draw();
+      this.dx = -gameSpeed;
+    }
+  
+    Draw () {
+  
+      ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+      // ctx.beginPath();
+      // ctx.fillStyle = this.c;
+      // ctx.fillRect(this.x, this.y, this.w, this.h);
+      // ctx.closePath();
+    }
+  }
+  
+  class Text {
+    constructor (t, x, y, a, c, s){
+      this.t = t;
+      this.x = x;
+      this.y = y;
+      this.a = a;
+      this.c = c;
+      this.s = s;
+    }
+    Draw() {
+      ctx.beginPath();
+      ctx.fillStyle = this.c;
+      ctx.font = this.s + "px sans-serif"
+      ctx.textAlign = this.a;
+      ctx.fillText(this.t, this.x, this.y);
+      ctx.closePath();
+    }
+  }
+  
+  
+  //funções
+  
+  function SpawnObstacle () {
+    let size = RandomIntInRange(20, 70);
+    let type = RandomIntInRange(0, 1);
+    let obstacle = new Obstacle(canvas.width + size, canvas.height - size, size, size, '#2484E4');
+  
+    if (type == 1) {
+      obstacle.y -= player.originalHeight - 10;
+    }
+    obstacles.push(obstacle);
+  }
+  
+  
+  function RandomIntInRange (min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+  
+  function Start() {
+    canvas.width = 905;
+    canvas.height = 404;
+  
+    ctx.font = "20px sans-serif";
+  
+    gameSpeed = 3;
+    gravity = 1;
+  
+    score = 0;
+    highscore = 0;
+    if (localStorage.getItem('highscore')) {
+      highscore = localStorage.getItem('highscore');
+    }
+  
+    player = new Player(80, 0, 50, 50);
+  
+    scoreText = new Text("Score: " + score, 25, 25, "left", "#212121", "20");
+    highscoreText = new Text("Highscore: " + highscore, canvas.width - 25, 25, "right", "#212121", "20");
+  
+    requestAnimationFrame(Update);
+  }
+  
+  
+  let initialSpawnTimer = 200;
+  let spawnTimer = initialSpawnTimer;
+  function Update () {
+    requestAnimationFrame(Update);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+    spawnTimer--;
+    if (spawnTimer <= 0) {
+      SpawnObstacle();
+      console.log(obstacles);
+      spawnTimer = initialSpawnTimer - gameSpeed * 8;
+      
+      if (spawnTimer < 60) {
+        spawnTimer = 60;
+      }
+    }
+  
+    //aqui vai gerar na tela os obstacles
+    for (let i = 0; i < obstacles.length; i++) {
+      let o = obstacles[i];
+  
+      if (o.x + o.w < 0) {
+        obstacles.splice(i, 1);
+      }
+  
+      if (
+        player.x < o.x + o.w &&
+        player.x + player.w > o.x &&
+        player.y < o.y + o.h &&
+        player.y + player.h > o.y
+      ) {
+        obstacles = [];
+        score = 0;
+        spawnTimer = initialSpawnTimer;
+        gameSpeed = 3;
+        window.localStorage.setItem('highscore', highscore);
+      }
+  
+      o.Update();
+    }
+  
+    player.Animate();
+  
+    score++;
+    scoreText.t = "Score: " + score;
+    scoreText.Draw();
+  
+    if (score > highscore) {
+      highscore = score;
+      highscoreText.t = "Highscore: " + highscore;
+    }
+    
+    highscoreText.Draw();
+  
+    gameSpeed += 0.003;
+  }
+  
+  Start();
+  
+  }
+  
